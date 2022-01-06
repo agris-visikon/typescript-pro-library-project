fb0c0f7bdf333ff18de724e02d41b39415a93d6e
Agris Jurjans <agris@visikon.com>
2022-01-06 11:24:13 +0100

    Adding the first feature

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,6 +1,20 @@
-import lib from './index'
+import { isBoolean } from './index'
 
-test('it should work', () => {
-  expect(true).toEqual(true)
-  console.log(lib)
+test('isBoolean should return true if the given value is boolean', () => {
+  expect(isBoolean(new Boolean())).toEqual(true)
+})
+
+test('isBoolean should return true if the given a literal `true`', () => {
+  expect(isBoolean(true)).toEqual(true)
+})
+
+test('isBoolean should return true if the given a literal `false`', () => {
+  expect(isBoolean(false)).toEqual(true)
+})
+
+test('isBoolean should return false if the given a non-boolean value', () => {
+  expect(isBoolean('')).toEqual(false)
+  expect(isBoolean(undefined)).toEqual(false)
+  expect(isBoolean(null)).toEqual(false)
+  expect(isBoolean(123)).toEqual(false)
 })
