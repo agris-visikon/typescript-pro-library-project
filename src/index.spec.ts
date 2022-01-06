e7a6faf14bd7bd1bd91530cfcabd64eae443bd84
Agris Jurjans <agris@visikon.com>
2022-01-06 12:51:07 +0100

    Fine tuning our types and tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,20 +1,22 @@
 import { isBoolean } from './index'
 
-test('isBoolean should return true if the given value is boolean', () => {
-  expect(isBoolean(new Boolean())).toEqual(true)
-})
+describe('isBoolean()', () => {
+  it('should return true if the given value is boolean', () => {
+    expect(isBoolean(new Boolean())).toEqual(true)
+  })
 
-test('isBoolean should return true if the given a literal `true`', () => {
-  expect(isBoolean(true)).toEqual(true)
-})
+  it('should return true if the given a literal `true`', () => {
+    expect(isBoolean(true)).toEqual(true)
+  })
 
-test('isBoolean should return true if the given a literal `false`', () => {
-  expect(isBoolean(false)).toEqual(true)
-})
+  it('should return true if the given a literal `false`', () => {
+    expect(isBoolean(false)).toEqual(true)
+  })
 
-test('isBoolean should return false if the given a non-boolean value', () => {
-  expect(isBoolean('')).toEqual(false)
-  expect(isBoolean(undefined)).toEqual(false)
-  expect(isBoolean(null)).toEqual(false)
-  expect(isBoolean(123)).toEqual(false)
+  it('should return false if the given a non-boolean value', () => {
+    expect(isBoolean('')).toEqual(false)
+    expect(isBoolean(undefined)).toEqual(false)
+    expect(isBoolean(null)).toEqual(false)
+    expect(isBoolean(123)).toEqual(false)
+  })
 })
